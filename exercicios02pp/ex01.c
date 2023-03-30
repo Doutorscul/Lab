d56e74bf66375e06b93642266ff363bb4e6f0a2f
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-03-30 12:39:24 -0300

    Update ex01.c

---
+++ exercicios02pp/ex01.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <stdio.h>
+
+int main() {
+    int num;
+    printf("Digite um número inteiro: ");
+    scanf("%d", &num);
+    printf("O número digitado foi: %d", num);
+    return 0;
+
+}
