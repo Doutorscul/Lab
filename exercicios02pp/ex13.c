e6bee77bc39f003589f8699443402ecb2e05fcdf
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-03-30 12:41:53 -0300

    Update ex13.c

---
+++ exercicios02pp/ex13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    float nota1, nota2, nota3, media;
+    printf("Digite a nota da primeira prova: ");
+    scanf("%f", &nota1);
+    printf("Digite a nota da segunda prova: ");
+    scanf("%f", &nota2);
+    printf("Digite a nota da terceira prova: ");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + (nota3 * 2)) / 4;
+
+    if (media >= 60) {
+        printf("Parabéns! Você foi aprovado com média %.2f\n", media);
+    } else {
+        printf("Infelizmente você foi reprovado com média %.2f\n", media);
+    }
+
+    return 0;
+}
