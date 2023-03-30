389a36bae3ec93917435ba34e2b7b861a2e3fc2a
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-03-30 12:46:15 -0300

    Update ex23.c

---
+++ exercicios02pp/ex23.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+   int ano;
+
+   printf("Digite o ano: ");
+   scanf("%d", &ano);
+
+   if ((ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)) {
+      printf("%d é um ano bissexto.\n", ano);
+   } else {
+      printf("%d não é um ano bissexto.\n", ano);
+   }
+
+   return 0;
+}
