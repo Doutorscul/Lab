8f08139480abe30439392c38e770347f5db3954a
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-12 12:38:12 -0300

    Update exercicio03.c

--- exercicios03pp/exercicio03.c
+++ exercicios03pp/exercicio03.c
@@ -11,15 +11,3 @@ int main() {
 
     return 0;
 }
-4- #include <stdio.h>
-
-int main() {
-    int contador = 0;  
-
-    while (contador <= 100000) {
-        printf("%d\n", contador);  
-        contador += 1000;  
-    }
-
-    return 0;
-}
