bbee6d2a977ac396996b0cdd84ce51e995a5c270
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-12 19:13:33 -0300

    Atualizar o exercicio10.c

--- exercicios03pp/exercicio10.c
+++ exercicios03pp/exercicio10.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int i;        // Variável de controle para o loop
-    int soma = 0; // Variável para armazenar a soma dos números pares
+    int i;        
+    int soma = 0;
 
-    // Calcular a soma dos 50 primeiros números pares
+   
     for (i = 1; i <= 50; i++) {
-        soma += 2 * i; // Fórmula para calcular números pares: 2 * i
+        soma += 2 * i;
     }
 
-    // Exibir a soma dos 50 primeiros números pares
+
     printf("A soma dos 50 primeiros numeros pares eh: %d\n", soma);
 
     return 0;
