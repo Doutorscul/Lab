3c6fd1ceaa105e0179a27915ef2c0b6d520b18a5
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-12 19:07:29 -0300

    Atualizar o exercicio23.c

--- exercicios03pp/exercicio23.c
+++ exercicios03pp/exercicio23.c
@@ -3,18 +3,18 @@
 int main() {
     int n;
 
-    // Lê o número inteiro positivo n
+    
     printf("Digite um numero inteiro positivo: ");
     scanf("%d", &n);
 
-    int num = 1; // Número a ser impresso
-    int linha, coluna; // Variáveis de controle para as linhas e colunas do Triângulo de Floyd
+    int num = 1; 
+    int linha, coluna;
 
-    // Loop para imprimir n linhas do Triângulo de Floyd
+   
     for (linha = 1; linha <= n; linha++) {
         for (coluna = 1; coluna <= linha; coluna++) {
             printf("%d ", num);
-            num++; // Incrementa o número a ser impresso
+            num++;
         }
         printf("\n");
     }
