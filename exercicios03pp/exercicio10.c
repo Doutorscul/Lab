d022915899f5258ab9d6784094de16b50361a4b7
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-13 13:40:20 -0300

    Atualizar o exercicio10.c

--- exercicios03pp/exercicio10.c
+++ exercicios03pp/exercicio10.c
@@ -4,11 +4,9 @@ int main() {
     int i;        
     int soma = 0;
 
-   
     for (i = 1; i <= 50; i++) {
         soma += 2 * i;
     }
-
 
     printf("A soma dos 50 primeiros numeros pares eh: %d\n", soma);
 
