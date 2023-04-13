957e2cbb31e1aa8c8c77c44f8028ff90ebe45264
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-13 13:39:54 -0300

    Atualizar o exercicio09.c

--- exercicios03pp/exercicio09.c
+++ exercicios03pp/exercicio09.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int n; // Variável para armazenar o número inteiro N
-    int i; // Variável de controle para o loop
-
-    // Solicitar ao usuário que digite um número inteiro N
+    int n;  
+    int i;
+
     printf("Digite um numero inteiro N: ");
     scanf("%d", &n);
-
-    // Verificar se N é positivo
+
     if (n <= 0) {
         printf("Erro: N deve ser um numero inteiro positivo maior que zero.\n");
-        return 1; // Encerrar o programa com código de erro
+        return 1; 
     }
 
-    // Imprimir os N primeiros números naturais ímpares
+    
     printf("Os primeiros %d numeros naturais impares sao: ", n);
     for (i = 1; i <= n; i++) {
         printf("%d ", 2 * i - 1); // Fórmula para calcular números ímpares: 2*i - 1
