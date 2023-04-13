da8d25f58b95cc4b293dd7cd066eee171403e680
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-13 13:35:46 -0300

    Atualizar o exercicio15.c

--- exercicios03pp/exercicio15.c
+++ exercicios03pp/exercicio15.c
@@ -2,25 +2,25 @@
 #include <math.h> 
 
 int main() {
-    double valor; // valor lido
-    double quadrado, cubo, raizQuadrada; // variáveis para armazenar os resultados
+    double valor; 
+    double quadrado, cubo, raizQuadrada; 
 
-    // Loop para leitura dos valores
+    
     while (1) {
         printf("Digite um valor (ou um valor negativo/zero para finalizar): ");
         scanf("%lf", &valor);
 
         if (valor <= 0) {
-            // Se valor for negativo ou zero, finaliza o loop
+            
             break;
         }
 
-        // Cálculos
+       
         quadrado = valor * valor;
         cubo = valor * valor * valor;
         raizQuadrada = sqrt(valor);
 
-        // Exibição dos resultados
+       
         printf("Valor: %.2lf\n", valor);
         printf("Quadrado: %.2lf\n", quadrado);
         printf("Cubo: %.2lf\n", cubo);
