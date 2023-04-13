cc8540e6191975045886cd729535eeccc0c3af6b
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-13 13:33:44 -0300

    Atualizar o exercicio17.c

--- exercicios03pp/exercicio17.c
+++ exercicios03pp/exercicio17.c
@@ -1,14 +1,14 @@
  #include <stdio.h>
 
 int main() {
-    float altura_chico = 1.50; // altura inicial de Chico em metros
-    float altura_ze = 1.10; // altura inicial de Zé em metros
-    int anos = 0; // contador de anos
+    float altura_chico = 1.50; 
+    float altura_ze = 1.10; 
+    int anos = 0; 
 
-    // Loop para calcular quantos anos serão necessários para que Zé seja mais alto que Chico
+    
     while (altura_ze <= altura_chico) {
-        altura_chico += 0.02; // Chico cresce 2 cm por ano (ou 0.02 metros)
-        altura_ze += 0.03; // Zé cresce 3 cm por ano (ou 0.03 metros)
+        altura_chico += 0.02; (ou 0.02 metros)
+        altura_ze += 0.03; (ou 0.03 metros)
         anos++;
     }
 
