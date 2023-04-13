0e48cea85a2e1ba2e4b07878c60302b2b163a55e
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-13 13:38:02 -0300

    Atualizar o exercicio12.c

--- exercicios03pp/exercicio12.c
+++ exercicios03pp/exercicio12.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int num;    // Variável para armazenar o número digitado
-    int soma = 0; // Variável para armazenar a soma dos divisores
+    int num;   
+    int soma = 0; 
 
-    // Ler o número do usuário
     printf("Digite um numero inteiro: ");
     scanf("%d", &num);
-
-    // Calcular a soma dos divisores do número
+
     for (int i = 1; i < num; i++) {
         if (num % i == 0) {
             soma += i;
         }
     }
 
-    // Exibir a soma dos divisores do número
+    
     printf("A soma dos divisores de %d (excluindo ele proprio) eh: %d\n", num, soma);
 
     return 0;
