4768d0466a4c388684d3a50b538918e20da97544
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-13 13:31:09 -0300

    Atualizar o exercicio22.c

--- exercicios03pp/exercicio22.c
+++ exercicios03pp/exercicio22.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-// Função para verificar se um número é palíndromo
+
 int ehPalindromo(int numero) {
     int numeroOriginal = numero;
     int numeroInvertido = 0;
 
-    // Inverte o número
+    
     while (numero > 0) {
         int digito = numero % 10;
         numeroInvertido = numeroInvertido * 10 + digito;
         numero /= 10;
     }
 
-    // Verifica se o número original é igual ao número invertido
+    
     if (numeroOriginal == numeroInvertido) {
-        return 1; // É palíndromo
+        return 1; 
     } else {
-        return 0; // Não é palíndromo
+        return 0; 
     }
 }
 
 int main() {
     int maiorPalindromo = 0;
 
-    // Percorre todos os produtos de dois números de 3 dígitos
+    
     for (int i = 100; i <= 999; i++) {
         for (int j = i; j <= 999; j++) {
             int produto = i * j;
@@ -33,7 +33,7 @@ int main() {
         }
     }
 
-    // Exibe o maior número palíndromo encontrado
+    
     printf("O maior número palíndromo feito a partir do produto de dois números de 3 dígitos é: %d\n", maiorPalindromo);
 
     return 0;
