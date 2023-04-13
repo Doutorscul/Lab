77a3c2544e37407a77719a5f433dfced38079c5f
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-13 13:38:35 -0300

    Atualizar o exercicio11.c

--- exercicios03pp/exercicio11.c
+++ exercicios03pp/exercicio11.c
@@ -1,14 +1,12 @@
 11- #include <stdio.h>
 
 int main() {
-    int i; // Variável de controle para o loop
-    int n; // Variável para armazenar o valor de N
-
-    // Ler o valor de N
+    int i;
+    int n; 
+
     printf("Digite um numero inteiro positivo N: ");
     scanf("%d", &n);
-
-    // Exibir os números naturais de 0 até N em ordem crescente
+
     printf("Numeros naturais de 0 ate %d em ordem crescente:\n", n);
     for (i = 0; i <= n; i++) {
         printf("%d\n", i);
