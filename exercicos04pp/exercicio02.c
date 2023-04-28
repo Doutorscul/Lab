dfd550d9981dc5f41e1de2ff99897cdfb3623926
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-27 22:52:40 -0300

    Atualizar o exercicio02.c

---
+++ exercicos04pp/exercicio02.c
@@ -0,0 +1,28 @@
+2- #include <stdio.h>
+
+int main() {
+    int i; 
+
+    printf("Usando o loop for:\n");
+    for (i = 1; i <= 100; i++) {
+        printf("%d ", i);  
+    }
+    printf("\n");
+
+    printf("\nUsando o loop while:\n");
+    i = 1;  
+    while (i <= 100) {
+        printf("%d ", i); 
+        i++; 
+    }
+    printf("\n");
+
+    printf("\nUsando o loop do-while:\n");
+    i = 1; 
+    do {
+        printf("%d ", i);  
+        i++;  
+    } while (i <= 100);
+
+    return 0;
+}
