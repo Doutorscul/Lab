a2d27407f29963f23a86339e413f8ffe6ae201f6
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-27 22:56:30 -0300

    Atualizar o exercicio06.c

---
+++ exercicos04pp/exercicio06.c
@@ -0,0 +1,26 @@
+6- #include <stdio.h>
+
+int main() {
+    int numeros[10];  
+    int soma = 0;    
+    float media;     
+    int i;           
+
+    printf("Digite 10 numeros inteiros:\n");
+    for (i = 0; i < 10; i++) {
+        printf("Numero %d: ", i + 1);
+        scanf("%d", &numeros[i]);  
+    }
+
+    for (i = 0; i < 10; i++) {
+        soma += numeros[i];  
+    }
+
+    
+    media = (float) soma / 10;  
+
+    
+    printf("A media dos 10 numeros e: %.2f\n", media);
+
+    return 0;
+}
