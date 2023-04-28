b024df60255ddbc0a83fb1a13b7575d683bf1a35
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-27 23:05:48 -0300

    Atualizar o exercicio13.c

---
+++ exercicos04pp/exercicio13.c
@@ -0,0 +1,41 @@
+1l #include <stdio.h>
+
+float calculadora(float num1, float num2, char operador) {
+    float resultado;
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("Erro: divisão por zero\n");
+                resultado = 0;
+            } else {
+                resultado = num1 / num2;
+            }
+            break;
+        default:
+            printf("Operador inválido\n");
+            resultado = 0;
+            break;
+    }
+    return resultado;
+}
+
+int main() {
+    float num1, num2, resultado;
+    char operador;
+    printf("Digite dois números: ");
+    scanf("%f %f", &num1, &num2);
+    printf("Digite o operador (+, -, *, /): ");
+    scanf(" %c", &operador);
+    resultado = calculadora(num1, num2, operador);
+    printf("Resultado: %.2f\n", resultado);
+    return 0;
+}
