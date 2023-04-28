b6c3408aaacd008965319bc4d9c2c4d46915f9b0
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-27 23:25:54 -0300

    Atualizar o exercicio22.c

--- exercicos04pp/exercicio22.c
+++ exercicos04pp/exercicio22.c
@@ -1,2 +1,29 @@
 **Questão 23**
 
+#include <stdio.h>
+
+void gerar_triangulo_lateral(int n) {
+  int i, j;
+
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= i; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+  for (i = n-1; i >= 1; i--) {
+    for (j = 1; j <= i; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+}
+
+int main() {
+  int n;
+  printf("Digite um numero inteiro positivo: ");
+  scanf("%d", &n);
+  gerar_triangulo_lateral(n);
+  return 0;
+}
