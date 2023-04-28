277e873512e19cfff16ed1eb8e397bd45721e190
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-04-27 22:51:35 -0300

    Atualizar o exercicio01.c

---
+++ exercicos04pp/exercicio01.c
@@ -0,0 +1,21 @@
+1 - #include <stdio.h>
+
+int main() {
+    int num = 1; 
+    int count = 0; 
+
+    printf("Os cinco primeiros multiplos de 3 sao: \n");
+
+    
+    while (count < 5) {
+        if (num % 3 == 0) {  
+            printf("%d ", num); 
+            count++; 
+        }
+        num++; 
+    }
+
+    printf("\n");
+
+    return 0;
+}
