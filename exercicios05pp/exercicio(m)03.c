f007a4785d74b333a7b14d720160b4323e97697a
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-05-04 23:47:28 -0300

    Atualizar o exercicio(m)03.c

--- exercicios05pp/exercicio(m)03.c
+++ exercicios05pp/exercicio(m)03.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX 100
 
 int main() {
-    int matriz[4][4], maior = matriz[0][0], linha_maior, coluna_maior, i, j;
+    char palavra[MAX];
+    int i, tam;
     
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra);
     
-    printf("Digite os elementos da matriz 4x4:\n");
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            scanf("%d", &matriz[i][j]);
-            if (matriz[i][j] > maior) {
-                maior = matriz[i][j];
-                linha_maior = i;
-                coluna_maior = j;
-            }
-        }
+    tam = strlen(palavra);
+    
+    printf("Palavra invertida: ");
+    for (i = tam - 1; i >= 0; i--) {
+        printf("%c", palavra[i]);
     }
-    
-    
-    printf("\nMatriz digitada:\n");
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-    
-    
-    printf("\nO maior valor encontrado foi %d, na linha %d e coluna %d.", maior, linha_maior, coluna_maior);
+    printf("\n");
     
     return 0;
 }
