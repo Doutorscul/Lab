f42a4ef777ea14d3664ce851ec74eb10587969f4
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-05-04 23:50:33 -0300

    Atualizar o exercicio(m)05.c

--- exercicios05pp/exercicio(m)05.c
+++ exercicios05pp/exercicio(m)05.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int matriz[10][10], i, j;
-    
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            if (i < j) {
-                matriz[i][j] = 2*i + 7*j - 2;
-            } else if (i == j) {
-                matriz[i][j] = 3*i*i - 1;
-            } else {
-                matriz[i][j] = 4*i*i*i - 5*j*j + 1;
-            }
+    char palavra[50];
+    int i;
+
+    printf("Digite uma palavra em letras maiusculas: ");
+    scanf("%s", palavra);
+
+    for (i = 0; i < strlen(palavra); i++) {
+        if (palavra[i] >= 65 && palavra[i] <= 90) { 
+            palavra[i] = palavra[i] + 32;
         }
     }
-    
-    printf("Matriz gerada:\n");
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            printf("%5d", matriz[i][j]);
-        }
-        printf("\n");
-    }
-    
+
+    printf("A palavra em letras minusculas e: %s\n", palavra);
+
     return 0;
 }
